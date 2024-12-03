03bf22351e4e715527523bb1dd0b27435a0b3c22
ClankJake <44306273+ClankJake@users.noreply.github.com>
2024-12-03 16:44:42 -0300

    Update notify_whatsapp.py
    Deixei o codigo mais organizado para adicionar novos argumentos

--- notify_whatsapp.py
+++ notify_whatsapp.py
@@ -12,8 +12,9 @@ Tautulli > Settings > Notification Agents > Scripts > Gear icon:
         Playback Recently Added: webhook_notify.py
 Tautulli > Settings > Notifications > Script > Script Arguments:
         -sn {show_name} -ena {episode_name} -ssn {season_num00} -enu {episode_num00} -dur {duration}
-        -srv {server_name} -med {media_type} -tt {title} -purl {plex_url} -pos {poster_url}
-        -genres {genres} -rating {rating} -summary {summary} -year {year} -lname {library_name}
+        -med {media_type} -tt {title} -pos {poster_url} -genres {genres} -rating {rating} 
+        -summary {summary} -year {year} -lname {library_name} 
+        -vr {video_resolution} -cr {content_rating}
 """
 
 from __future__ import unicode_literals
@@ -23,147 +24,142 @@ import os
 import time
 from urllib.parse import urlparse
 
-# ## EDIT THESE SETTINGS ##
-WEBHOOK_URL = 'http://you_ip:3000/send/image'
-PHONE = '"CODIGO DO CANAL"@newsletter'
-LOG_FILE_PATH = '/config/notfify_whatsapp.log'
+# Configurações gerais
+CONFIG = {
+    "webhook_url": 'http://you_ip:3000/send/image',
+    "phone": 'you_id@s.whatsapp.net', # Para Canal: @newsletter, Grupo: g.us, Privado: @s.whatsapp.net
+    "log_file_path": '/config/notify_whatsapp.log',
+    "retry_delay": 10,
+    "max_retries": 3
+}
 
-MOVIE_TEXT = "🎬  *Título:* {title} ({year})\n🕓  *Duração:* {duration} minutos\n🎭  *Gênero:* {genres}\n📺  *Biblioteca:* {library_name}\n\n⭐ *{rating}* | {summary}"
-TV_TEXT = "📺  *Série:* {show_name} ({year})\n🔢  *Episódio:* {season_num00}x{episode_num00} - {episode_name}\n🕓  *Duração:* {duration} minutos\n🎭  *Gênero:* {genres}\n📺  *Biblioteca:* {library_name}\n\n*Sinopse:*\n⭐ *{rating}* | {summary}"
-SHOW_TEXT = "📺  *Séries:* {show_name} ({year})\n🎭  *Gênero:* {genres}\n📺  *Biblioteca:* {library_name}\n\n*Sinopse:*\n⭐ *{rating}* | {summary}"
-SEASON_TEXT = "📺  *Série:* {show_name}\n📺  *Temporada:* {season_num00}\n🎭  *Gênero:* {genres}\n📺  *Biblioteca:* {library_name}\n\n*Sinopse:*\n⭐ *{rating}* | {summary}"
+TEMPLATES = {
+    "movie": "🎬  *Título:* {title} ({year})\n🕓  *Duração:* {duration} minutos\n🎭  *Gênero:* {genres}\n📺  *Biblioteca:* {library_name}\n\n⭐ *{rating}* | {summary}",
+    "episode": "📺  *Série:* {show_name} ({year})\n🔢  *Episódio:* {season_num}x{episode_num} - {episode_name}\n🕓  *Duração:* {duration} minutos\n🎭  *Gênero:* {genres}\n📺  *Biblioteca:* {library_name}\n\n⭐ *{rating}* | {summary}",
+    "show": "📺  *Séries:* {show_name} ({year})\n🎭  *Gênero:* {genres}\n📺  *Biblioteca:* {library_name}\n\n⭐ *{rating}* | {summary}",
+    "season": "📺  *Série:* {show_name}\n📺  *Temporada:* {season_num}\n🎭  *Gênero:* {genres}\n📺  *Biblioteca:* {library_name}\n\n⭐ *{rating}* | {summary}"
+}
 
 def log(message, log_enabled):
+    """Log message to file if logging is enabled."""
     if log_enabled:
-        with open(LOG_FILE_PATH, 'a') as log_file:
+        with open(CONFIG['log_file_path'], 'a') as log_file:
             log_file.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
 
-def download_image(url, filename, max_retries=3, retry_delay=10, log_enabled=False):
-    for attempt in range(max_retries):
+def download_image(url, filename, log_enabled):
+    """Download an image from a URL."""
+    for attempt in range(CONFIG['max_retries']):
         try:
-            request = requests.get(url, stream=True)
-            if request.status_code == 200:
+            response = requests.get(url, stream=True)
+            if response.status_code == 200:
                 with open(filename, 'wb') as image:
-                    for chunk in request.iter_content(1024):
+                    for chunk in response.iter_content(1024):
                         image.write(chunk)
                 log(f"Successfully downloaded image: {url}", log_enabled)
                 return True
-            elif request.status_code == 429:
-                log(f"Failed to download image: HTTP {request.status_code}. Retrying in {retry_delay} seconds...", log_enabled)
-                time.sleep(retry_delay)
+            elif response.status_code == 429:
+                log(f"Rate limited: Retrying in {CONFIG['retry_delay']} seconds...", log_enabled)
+                time.sleep(CONFIG['retry_delay'])
             else:
-                log(f"Failed to download image: HTTP {request.status_code}", log_enabled)
+                log(f"Failed to download image: HTTP {response.status_code}", log_enabled)
                 return False
         except Exception as e:
             log(f"Error downloading image: {e}", log_enabled)
-            if attempt < max_retries - 1:
-                log(f"Retrying in {retry_delay} seconds...", log_enabled)
-                time.sleep(retry_delay)
+            if attempt < CONFIG['max_retries'] - 1:
+                time.sleep(CONFIG['retry_delay'])
             else:
                 return False
 
-if __name__ == '__main__':
+def build_arguments():
+    """Build and parse command-line arguments."""
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-sn', '--show_name', action='store', default='',
-                        help='The name of the TV show')
-    parser.add_argument('-ena', '--episode_name', action='store', default='',
-                        help='The name of the episode')
-    parser.add_argument('-ssn', '--season_num', action='store', default='',
-                        help='The season number of the TV show')
-    parser.add_argument('-enu', '--episode_num', action='store', default='',
-                        help='The episode number of the TV show')
-    parser.add_argument('-dur', '--duration', action='store', default='',
-                        help='The duration of the content')
-    parser.add_argument('-srv', '--plex_server', action='store', default='',
-                        help='The name of the Plex server')
-    parser.add_argument('-med', '--media_type', action='store', default='',
-                        help='The media type (e.g., movie, episode, season, show)')
-    parser.add_argument('-tt', '--title', action='store', default='',
-                        help='The title of the media')
-    parser.add_argument('-purl', '--plex_url', action='store', default='',
-                        help='URL to Plex video')
-    parser.add_argument('-pos', '--poster', action='store', default='',
-                        help='The poster URL')
-    parser.add_argument('-genres', '--genres', action='store', default='',
-                        help='Genres of the media')
-    parser.add_argument('-rating', '--rating', action='store', default='',
-                        help='Rating of the media')
-    parser.add_argument('-summary', '--summary', action='store', default='',
-                        help='Summary of the media')
-    parser.add_argument('-year', '--year', action='store', default='',
-                        help='Release year of the media')
-    parser.add_argument('-lname', '--library_name', action='store', default='',
-                        help='Library media')
-    parser.add_argument('-log', '--log_enabled', action='store_true',
-                        help='Enable logging')
+    # Lista de argumentos
+    arguments = [
+        ('-med', '--media_type', 'Media type (e.g., movie, episode)', '', None),
+        ('-tt', '--title', 'Media title', '', None),
+        ('-sn', '--show_name', 'TV show name', '', None),
+        ('-ena', '--episode_name', 'Episode name', '', None),
+        ('-ssn', '--season_num', 'Season number', '', None),
+        ('-enu', '--episode_num', 'Episode number', '', None),
+        ('-dur', '--duration', 'Duration', '', None),
+        ('-genres', '--genres', 'Genres', '', None),
+        ('-rating', '--rating', 'Rating', '', None),
+        ('-summary', '--summary', 'Summary', '', None),
+        ('-year', '--year', 'Release year', '', None),
+        ('-lname', '--library_name', 'Library name', '', None),
+        ('-pos', '--poster', 'Poster URL', '', None),
+        ('-cr', '--content_rating', 'Content Rating', '', None),
+        ('-vr', '--video_resolution', 'Video Resolution', '', None),
+        ('-log', '--log_enabled', 'Enable logging', False, 'store_true'),
+    ]
 
-    p = parser.parse_args()
-    log_enabled = p.log_enabled
+    # Adiciona os argumentos ao parser
+    for short, long, help_text, default, action in arguments:
+        if action:
+            parser.add_argument(short, long, help=help_text, default=default, action=action)
+        else:
+            parser.add_argument(short, long, help=help_text, default=default)
 
-    log("Script started", log_enabled)
-    log(f"Arguments parsed: {p}", log_enabled)
+    # Retorna os argumentos analisados
+    return parser.parse_args()
 
-    if p.media_type == 'movie':
-        BODY_TEXT = MOVIE_TEXT.format(
-            media_type=p.media_type, title=p.title, duration=p.duration, genres=p.genres, rating=p.rating,
-            summary=p.summary, year=p.year, library_name=p.library_name)
-    elif p.media_type == 'episode':
-        BODY_TEXT = TV_TEXT.format(
-            media_type=p.media_type, show_name=p.show_name, title=p.title,
-            season_num00=p.season_num, episode_num00=p.episode_num, episode_name=p.episode_name,
-            duration=p.duration, genres=p.genres, rating=p.rating, summary=p.summary, year=p.year, library_name=p.library_name)
-    elif p.media_type == 'show':
-        BODY_TEXT = SHOW_TEXT.format(
-            media_type=p.media_type, title=p.title,
-            genres=p.genres, rating=p.rating, summary=p.summary, year=p.year, show_name=p.show_name, library_name=p.library_name)
-    elif p.media_type == 'season':
-        BODY_TEXT = SEASON_TEXT.format(
-            media_type=p.media_type, show_name=p.show_name, title=p.title, season_num00=p.season_num,
-            genres=p.genres, rating=p.rating, summary=p.summary, year=p.year, library_name=p.library_name)
-    else:
-        log("Unsupported media type, exiting.", log_enabled)
-        exit()
-
-    # Extract the filename from the URL
-    parsed_url = urlparse(p.poster)
-    original_filename = os.path.basename(parsed_url.path)
-    if not original_filename:
-        original_filename = 'temp_image'
-    if '.' not in original_filename:
-        original_filename += '.jpg'  # Default to .jpg if no extension found
-
-    original_image_path = original_filename
-
-    image_downloaded = download_image(p.poster, original_image_path, log_enabled=log_enabled)
-
-    # Envia os dados para o webhook
-    if image_downloaded:
-        log("Image ready, preparing to send webhook", log_enabled)
-        with open(original_image_path, 'rb') as image_file:
-            files = {
-                'image': (original_image_path, image_file, 'image/jpeg')
-            }
+def send_webhook(body_text, image_path, log_enabled):
+    """Send data to the webhook."""
+    try:
+        with open(image_path, 'rb') as image_file:
+            files = {'image': (image_path, image_file, 'image/jpeg')}
             data = {
-                'phone': PHONE,
-                'caption': BODY_TEXT,
+                'phone': CONFIG['phone'],
+                'caption': body_text,
                 'compress': 'true',
                 'view_once': 'false'
             }
-            response = requests.post(WEBHOOK_URL, data=data, files=files)
+            response = requests.post(CONFIG['webhook_url'], data=data, files=files)
             log(f"Webhook response: {response.status_code} - {response.text}", log_enabled)
-
-        # Verifica a resposta
-        print(response.status_code)
-
-    log("Cleaning up images", log_enabled)
-
-    # Delete the images after sending the webhook
-    try:
-        if os.path.exists(original_image_path):
-            os.remove(original_image_path)
-            log(f"Deleted original image: {original_image_path}", log_enabled)
+            return response.status_code
     except Exception as e:
-        log(f"Error deleting images: {e}", log_enabled)
+        log(f"Error sending webhook: {e}", log_enabled)
+        return None
+
+if __name__ == '__main__':
+    args = build_arguments()
+    log_enabled = args.log_enabled
+
+    log("Script started", log_enabled)
+    log(f"Arguments: {args}", log_enabled)
+
+    if args.media_type not in TEMPLATES:
+        log("Unsupported media type", log_enabled)
+        exit()
+
+    # Prepare the body text
+    body_text = TEMPLATES[args.media_type].format(
+        title=args.title, show_name=args.show_name, episode_name=args.episode_name,
+        season_num=args.season_num, episode_num=args.episode_num, duration=args.duration,
+        genres=args.genres, rating=args.rating, summary=args.summary, year=args.year,
+        library_name=args.library_name, video_resolution=args.video_resolution, 
+        content_rating=args.content_rating
+    )
+
+    parsed_url = urlparse(args.poster)
+    original_filename = os.path.basename(parsed_url.path)
+
+    # Define um nome padrão caso o arquivo não tenha nome na URL
+    if not original_filename:
+        original_filename = 'temp_image.jpg'
+    elif '.' not in original_filename:
+        original_filename += '.jpg'  # Adiciona .jpg se não houver extensão
+
+    original_image_path = original_filename
+
+    # Faz o download da imagem
+    image_downloaded = download_image(args.poster, original_image_path, log_enabled=args.log_enabled)
+
+    # Send the webhook
+    if image_downloaded:
+        send_webhook(body_text, original_filename, log_enabled)
+        os.remove(original_filename)
+        log("Cleaned up downloaded image", log_enabled)
 
     log("Script completed", log_enabled)
